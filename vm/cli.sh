661425f19abee87d7daafea6c515d9464e39f515
Giuseppe SCARAMUZZINO <giuseppe.scaramuzzino@amadeus.com>
2026-01-09 15:34:03 +0100

    Add Azure deployment scripts for virtual network and VM setup

---
+++ vm/cli.sh
@@ -0,0 +1,3 @@
+az group create --name rg-ita-az-104-vm --location italynorth
+
+
--- vnet/cli.sh
+++ vnet/cli.sh
@@ -2,11 +2,62 @@ $RESOURCE_GROUP=""
 $VNET_NAME=""
 az network vnet delete --resource-group $RESOURCE_GROUP --name $VNET_NAME
 
+az group create --name rg-ita-az-104-vnet --location italynorth
 
-az group create --name rg-eastus-az-104-vnet --location eastus
+az network public-ip create \
+  --name- ip-public-1 \
+  --resource-group rg-ita-az-104-vnet \
+  --tags 'Learning=AZ-104' \
+  --version IpV4 \
+  --sku standard \
+  --location italynorth \
+  --allocation-method Static
+
 
 az deployment group create \
-  --name DeployVnet \
-  --resource-group rg-eastus-az-104-vnet \
-  --template-file
-  --parameters
+  --name DeployVnet_1 \
+  --resource-group rg-ita-az-104-vnet \
+  --template-uri "https://raw.githubusercontent.com/gscaramuzzino/azure_by_doing/refs/heads/main/vnet/template.json" \
+  --parameters vnetName=vnet-1 subnetName=sbnet-1 vnetAddressPrefix=10.0.0.0/16 subnetPrefix=10.0.0.0/20
+
+az deployment group create \
+  --name DeployVnet_2 \
+  --resource-group rg-ita-az-104-vnet \
+  --template-uri "https://raw.githubusercontent.com/gscaramuzzino/azure_by_doing/refs/heads/main/vnet/template.json" \
+  --parameters vnetName=vnet-2 subnetName=sbnet-1 vnetAddressPrefix=11.0.0.0/16 subnetPrefix=11.0.0.0/20
+
+az deployment group create \
+  --name DeployVnet_3 \
+  --resource-group rg-ita-az-104-vnet \
+  --template-uri "https://raw.githubusercontent.com/gscaramuzzino/azure_by_doing/refs/heads/main/vnet/template.json" \
+  --parameters vnetName=vnet-3 subnetName=sbnet-1 vnetAddressPrefix=12.0.0.0/16 subnetPrefix=12.0.0.0/20
+
+  az network vnet peering create --name vnet-1_to_vnet-2 \
+                                 --resource-group rg-ita-az-104-vnet \
+                                 --vnet-name vnet-1 \
+                                 --remote-vnet vnet-2 \
+                                 --allow-forwarded-traffic true \
+                                 --allow-vnet-access true
+
+   az network vnet peering create --name vnet-1_to_vnet-2 \
+                                   --resource-group rg-ita-az-104-vnet \
+                                   --vnet-name vnet-2 \
+                                   --remote-vnet vnet-1 \
+                                   --allow-forwarded-traffic true
+
+   az network vnet peering create --name vnet-2_to_vnet-3 \
+                                  --resource-group rg-ita-az-104-vnet \
+                                  --vnet-name vnet-2 \
+                                  --remote-vnet vnet-3 \
+                                  --allow-forwarded-traffic true \
+                                  --allow-vnet-access false
+
+    az network vnet peering create --name vnet-2_to_vnet-3 \
+                                    --resource-group rg-ita-az-104-vnet \
+                                    --vnet-name vnet-3 \
+                                    --remote-vnet vnet-2 \
+                                    --allow-forwarded-traffic true
+
+  az group delete --name rg-ita-az-104-vnet
+
+
